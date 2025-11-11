9e7e7ce1c7ac7d09b8dbbc5e498eb2d919ceee91
Lucas Milhão <lucasmilhao683@gmail.com>
2025-11-11 19:45:55 -0300

    Add files via upload

---
+++ Game.java
@@ -0,0 +1,25 @@
+import java.awt.BorderLayout;
+
+import javax.swing.*;
+
+public class Game {
+	public static void main(String[] args) {
+		int largura = 720;
+		int altura = 250;
+
+		JFrame frame = new JFrame();
+		Menu menu = new Menu();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(largura, altura);
+		frame.setLayout(new BorderLayout());
+		frame.setResizable(false);
+		
+		frame.add(menu);
+		frame.setLocationRelativeTo(null);
+		
+		
+		menu.requestFocus();
+		frame.pack();
+		frame.setVisible(true);
+	}
+}
